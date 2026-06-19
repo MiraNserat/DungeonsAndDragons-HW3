ca1b834833306ad6bfeec8c391bbd39f49347fa1
yahyal <yahyal@post.bgu.ac.il>
2026-06-19 18:54:54 +0300

    Merge branch 'GameBoard' into Devlop-

---
+++ src/GameManagement/Loader.java
@@ -0,0 +1,10 @@
+package GameManagement;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.List;
+
+public class Loader {
+
+}
